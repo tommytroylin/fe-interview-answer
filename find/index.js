34210ae8a9ca9dcf4eb8eb36d5faee3b98e2046d
Tommy Troy Lin <tommytroylin@gmail.com>
2016-07-14 17:33:12 +0800

    Solve Find & FindLast

---
+++ find/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ *
+ *  尝试完成如下功能：
+ *
+ *  var arr1 = [3, 6, 9];
+ *
+ *  var found1 = find(arr1, function(i){
+ *      return i > 5;
+ *  });
+ *
+ *  console.log(found1); // 6
+ *
+ *  var arr2 = [{ name: 'ShangHai', size: 6340.5 }, { name: 'BeiJing', size: 16410.54 }];
+ *
+ *  var found2 = find(arr2, function(i){
+ *      return i.name === 'ShangHai';
+ *  });
+ *
+ *  console.log(found2); // { name: 'ShangHai', size: 6340.5 }
+ *
+ **/
+var find = function (array, func) {
+  for (let e of array) {
+    if (func(e)) {
+      return e;
+    }
+  }
+  return null;
+};
+
+module.exports = find;
